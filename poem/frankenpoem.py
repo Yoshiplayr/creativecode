1ce29d1566cccf9208995e5323c2e804436b15ed
Yoshiplayr <54549408+Yoshiplayr@users.noreply.github.com>
2019-09-19 22:39:48 -0400

    Update frankenpoem.py

--- poem/frankenpoem.py
+++ poem/frankenpoem.py
@@ -1,41 +1,70 @@
+from google.colab import files
+
+uploaded = files.upload()
+for fn in uploaded.keys():
+  text = uploaded[fn].decode()
+  
+  
 import random
 from textblob import TextBlob
 
-# This looks for a file called "frankenstein.txt" 
-# in the same directory as my python notebook or script.
-# Replace "frankenstein.txt" with your file's name.
-# The text file's contents are stored in the variable "text"
-# These are comments
-with open('frankenstein.txt', 'r') as file:
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+with open('skyrim32.txt', 'r') as file:
     text = file.read()
 
-# This parses the text file contents into a TextBlob oject called "blob"
 blob = TextBlob(text)
 
-# Create two empty lists for storing adjectives and nouns
 adjectives = []
 nouns = []
 verbs = []
+adverbs = []
+propernouns = []
+prep = []
+perpro = []
+conj = []
+inter = []
+presverb = []
 
-# TextBlob parses the words and labels them with a part-of-speech tag.
-# This code loops through the whole text, checks for adjectives and nouns
-# and adds them to the appropriate list.
 for word,pos in blob.tags:
-    # print(word,pos)
     if (pos == 'JJ'):
         adjectives.append(word)
     if (pos == 'NN'):
         nouns.append(word)
-    if (pos == 'VB'):
+    if (pos == 'VBG'):
         verbs.append(word)
-
-# This generates an eight-line poem by pairing a random adjective 
-# with a random noun eight times and printing the pairs.
-for i in range(8):
-    a1 = random.choice(adjectives)
-    a2 = random.choice(adjectives)
-    n1 = random.choice(nouns)
-    n2 = random.choice(nouns)
-    n3 = random.choice(nouns)
-    vb = random.choice(verbs)
-    print(n1 + " " + a1 + " " +  vb  + " " + a2  + " " + n3)
+    if (pos == 'RB'):
+        adverbs.append(word)
+    if (pos == 'NNP'):
+        propernouns.append(word)
+    if (pos == 'IN'):
+        prep.append(word)
+    if (pos == 'PRP'):
+        perpro.append(word)    
+    if (pos == 'CC'):
+        conj.append(word)
+    if (pos == 'UH'):
+        inter.append(word)
+    if (pos == 'VBP'):
+        presverb.append(word)
+        
+for i in range(3):
+    a = random.choice(adjectives)
+    b = random.choice(nouns)
+    b2 = random.choice(nouns)
+    c = random.choice(verbs)
+    d = random.choice(adverbs)
+    e = random.choice(propernouns)
+    f = random.choice(prep)
+    g = random.choice(conj)
+    g2 = random.choice(conj)
+    e2 = random.choice(propernouns)
+    h = random.choice(inter)
+    i = random.choice(perpro)  
+    j = random.choice(presverb)
+    
+    print(e,'and',e2,c)
+    print(e,c,b)
+    print(e2,c,b2)
