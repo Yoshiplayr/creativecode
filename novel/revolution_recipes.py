93b9a1d0d097230feb64d318e4a345c9dff60599
Yoshiplayr <54549408+Yoshiplayr@users.noreply.github.com>
2019-11-24 19:28:22 -0500

    ElderScrollsVItest2

--- novel/revolution_recipes.py
+++ novel/revolution_recipes.py
@@ -1,43 +1,50 @@
-import random
+# This would install weasyprint and markovify if the # wasn't there, but github wouldn't update the changes with the ! there for some reason
+# !pip install weasyprint
+# !pip install markovify
+
+from weasyprint import HTML
+
+novel =''
 import markovify
-import dominate 
-from dominate.tags import *
-import pdfkit
-
-
-novel = ''
-
-with open("combined.txt") as f:
+with open("BunchofScriptsFINAL.txt") as f:
     text = f.read()
-    
-    
-# Build the model.
+
 text_model = markovify.Text(text)
 
-for i in range(3605):
-    
-    
-    novel += str(text_model.make_sentence()) + " "
-    
-    r = random.randint(0,100)
-    
-    if (r < 36):
-        novel += "\n\n"
-    
-#print(novel)
+# so, for whatever reason, if I set the range too high it would put half the text on one line, so I split it up into 3 to get all 50000 words
+for i in range(2000):
+    novel+=str(text_model.make_sentence())
 
-chunked = novel.split("\n\n")
+for i in range(2000):
+    novel+=str(text_model.make_sentence())
 
-doc = dominate.document(title='Recipes for Revolution!')
+for i in range(1500):
+    novel+=str(text_model.make_sentence())
 
-with doc.head:
-    style("body {background-color: #fff; color: #333; font-size: 15pt}")
-    
-with doc:
-    h1("Recipes for Revolution")
-    p("A NaNoGenMo 2018 Novel")
-    
-    for c in chunked:
-        p(c)
 
-pdfkit.from_string(str(doc.render()), 'revolution_recipe.pdf')
+print(novel)
+
+# It took me a while to figure out the formatting, but I was able to mess with
+# how many characters I had to get the title centered in the page
+
+html_template = f"""
+
+<html>
+  <head><strong>––––––––– • ––––––––  The Elder Scrolls VI: Barry B. Powers vs Zilla  –––––––– • –––––––––</strong>
+  <div> By: Todd Howard, J.R.R. Tolkien, Roland Emmerich, Saban, Jerry Seinfeld, and Mike Myers </div>
+  <div>––––––––– • ––––––––––––––––– • ––––––––––––––––– • ––––––––––––––––– • –––––––––––
+  </div>
+  </head>
+  <title>Skyrim</title>
+  <body><div>
+  {novel}</div>
+  <head><div>DRAGONBORN will Return in THE AVENGERS</div>
+  <div><strong>––––––––– • ––––––––  THE END  –––––––– • –––––––––</strong></div></head>
+  <style>
+   body {{
+     font-family: Times New Roman;
+     }} 
+</html>
+"""
+
+HTML(string=html_template).write_pdf("TheElderScrollsVIFinalVersion.pdf")
